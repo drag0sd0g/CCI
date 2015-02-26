6ada48e128a881f9bb6995d1d3ff1a6fb15ad3a3
Dragos Dogaru <dragosdog@gmail.com>
2015-02-26 13:21:36 +0000

    added basic maven structure
    added basic maven structure

---
+++ src/main/java/com/cci/arrstr/Driver.java
@@ -0,0 +1,7 @@
+package com.cci.arrstr;
+
+public class Driver {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/test/java/com/cci/arrstr/Tests.java
@@ -0,0 +1,11 @@
+package com.cci.arrstr;
+
+
+import org.junit.Test;
+
+public class Tests {
+    @Test
+    public void test1(){
+
+    }
+}
